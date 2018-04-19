1b57ed8a81186062d7541bd17018bcfe9f32f014
Brenton Moodley <brenton@brenton.wethinkcode.co.za>
2018-04-19 09:32:43 +0200

    hitting an end point with a parameter to access single row, error handling middleware working, next to set up routes

--- app.js
+++ app.js
@@ -2,12 +2,27 @@ const express = require('express');
 const app = express();
 const pool = require('./db');
 
-app.get('/monsters', (request, response) => {
+app.get('/monsters', (request, response, next) => {
 	pool.query('SELECT * FROM monsters ORDER BY id ASC', (err, res) => {
-		if (err) return console.log(err);
+		if (err) return next(err);
 
 		response.json(res.rows);
 	});	
+});
+
+app.get('/monsters/:id', (request, response, next) => {
+	const { id } = request.params;
+	//res.send(req.params.id);
+	pool.query('SELECT * FROM monsters WHERE id = $1', [id], (err, res) => {
+		if (err) return next(err);
+	
+		response.json(res.rows);
+	}); // array is 1 indexed (starts at 1))
+});
+
+// Middleware
+app.use((err, req, res, next) => {
+	res.json(err);
 });
 
 //const port = 3000; // moved to /bin/www
